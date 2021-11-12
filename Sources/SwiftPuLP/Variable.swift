616d2e26cdcff1f5202bad2aa7767fd575bb8af0
Michel Tilman <mtilman@acm.org>
2021-11-12 10:12:08 +0100

    Update Variable comments.

--- Sources/SwiftPuLP/Variable.swift
+++ Sources/SwiftPuLP/Variable.swift
@@ -10,58 +10,64 @@ import Foundation
 import PythonKit
 
 /**
- A variable represents a decision variable and may appear in the linear objective function and / or in linear constraints of a LP model.
+ A variable represents a linear programming decision variable and may appear in the objective function and / or in linear constraints of a linear programming model.
  
- Each variable should have a unique name.
-
- The range of values for a variable is defined by its ``Domain``. All domains represent Double numbers, but may further limit the range to integer numbers or binary numbers.
+ The range of values for a variable is defined in the first place by its ``Domain``. A domain represents a subset of Double, such as real numbers, or the binary numbers 0 and 1.
  
- The domain values may be further restricted to optional lower and upper bounds.
+ In addition, a variable may specify lower and / or upper bounds, for instance to limit the domain values to the integer numbers less than 10.
  
- It is possible to create variables with invalid names or invalid bounds / domain combinations. Use ``validationErrors`` to detect if a variable is valid.
+ See also: <doc:UsingVariables>.
  */
 public class Variable {
     
     // MARK: -
     
-    ////// Type of a variable id.
+    /// Type of a variable id.
+    /// Different variables have different ids.
     typealias Id = ObjectIdentifier
     
     // MARK: -
 
     /**
-     A domain specifies the range of (Double) values a variable can take.
+     A domain specifies the  values a variable can take.
      
-     Supported domains are:
-     - binary (values 0 and 1).
-     - real (aka continuous numbers in PuLP)
-     - integer (integer values)
+     Domains identify subsets of Double and correspond to PuLP categories.
      */
     public enum Domain {
 
-        case binary, real, integer
+        /// Allowed values are 0 and 1.
+        case binary
+        
+        /// Allowed values are the integer numbers.
+        case integer
 
+        /// Allowed values are all Double numbers.
+        /// Corresponds to the *continuous* category in PuLP.
+        case real
+        
     }
 
     // MARK: -
     
-    /// The unique name of the variable.
+    /// The name of the variable.
     ///
     /// Should not be empty and should not contain special characters.
+    ///
+    /// Should be unique in a single model.
     public let name: String
     
     /// Optional lower bound for the values (inclusive).
     ///
-    /// If present must not exceed a non-nil maximum.
-    /// If present must be 0 for binary variables.
+    /// If present should not exceed a non-nil maximum.
+    /// If present should be 0 for binary variables.
     let minimum: Double?
     
     /// Optional upper bound for the values (inclusive).
     ///
-    /// If present must be 1 for binary variables.
+    /// If present should be 1 for binary variables.
     let maximum: Double?
     
-    /// Domain of values for this variable.
+    /// Domain of this variable.
     let domain: Domain
     
     /// Answers a unique id for the variable.
@@ -71,15 +77,16 @@ public class Variable {
     
     // MARK: -
     
-    /// Creates a variable with given name and ``Domain``.
+    /// Creates a variable with given name and domain.
     /// The variable may optionally specify a lower and / or upper bound for its values.
-    /// Invalid names or invalid bounds / domain combinations may result in ``validationErrors``.
-    /// 
+    ///
+    /// > Note: The initializer does not fail if parameters are not correct. To check if the definition is valid use ``validationErrors``.
+    ///
     /// - Parameters:
-    ///   - name: Unique name of the variable. Should not be empty and should not contain any of the characters "-+[] ->/"
-    ///   - minimum: Specifies a lower bound for the variable if present. If present should not exceed a non-nil maximum.= and must be 0 for binary variables.
-    ///   - maximum: Specifies an upper bound for the variable if present. If present must be 1 for binary variables.
-    ///   - domain: Specifies the range of (Double) values the variable may assume. Default = .real.
+    ///   - name: Name of the variable. Should not be empty and should not contain any of the characters "-+[] ->/". Should be unique in a single model.
+    ///   - minimum: Specifies a lower bound for the variable if present (default = nil). If not nil should not exceed a non-nil maximum. Should be nil or 0 for binary variables. 
+    ///   - maximum: Specifies an upper bound for the variable if present (default = nil). Should be nil or 1 for binary variables.
+    ///   - domain: Specifies the subset of Double values the variable may assume (default = .real).
     public init(_ name: String, minimum: Double? = nil, maximum: Double? = nil, domain: Domain = .real) {
         self.name = name
         self.minimum = domain == .binary && minimum == nil ? 0 : minimum
@@ -92,21 +99,20 @@ public class Variable {
 // MARK: -
 
 /**
- PuLP expects only one object for all similarly-named variables.
- The variable cache allows clients to cache new LpVariables if not already present,
- otherwise the existing one is returned.
+ PuLP expects only one Python LpVariable instance for every occurrence of the same Swift variable.
+ The variable cache allows clients to create and cache new LpVariables if not yet present, otherwise the cached instance is returned.
  */
 class VariableCache {
     
     // MARK: -
     
-    // Links variable names to generated PuLP variables.
+    // Links variable ids to generated PuLP variables.
     private var cache = [Variable.Id: PythonObject]()
     
     // MARK: -
     
-    // Answers the cached PuLP variable.
-    // If none found, generates a new one and caches it.
+    // Answers the cached PuLP LpVariable.
+    // If none is found, generates a new one and caches it.
     fileprivate subscript(key: Variable.Id, default defaultValue: @autoclosure () -> PythonObject) -> PythonObject {
         get {
             return cache[key] ?? {
@@ -122,7 +128,7 @@ class VariableCache {
 // MARK: - PythonConvertible -
 
 /**
- Converting Variable into Python.
+ Converting a Variable into a Python object.
  */
 extension Variable: PythonConvertible {
     
@@ -137,8 +143,8 @@ extension Variable: PythonConvertible {
 
     /// Converts the variable into a PuLP LpVariable, optionally caching generated PuLP variables.
     ///
-    /// - Parameter cache: If present caches a generated Python LpVariable instance for each SwithPuLP variable.
-    /// - Returns: Cached or newly generated Python LpVariable.
+    /// - Parameter cache: If present caches the first generated PuLP LpVariable instance for each Variable.
+    /// - Returns: Cached or newly generated PuLP LpVariable.
     func pythonObject(withCache cache: VariableCache?) -> PythonObject {
         guard let cache = cache else { return pythonObject }
 
@@ -151,7 +157,7 @@ extension Variable: PythonConvertible {
 // MARK: -
 
 /**
- Converting Variable.Domain into Python.
+ Converting Variable.Domain into a Python object.
  */
 extension Variable.Domain: PythonConvertible {
 
@@ -182,7 +188,6 @@ extension Variable: Equatable {
     // MARK: -
 
     /// Returns true if the lhs and rhs variables are the same content-wise, false otherwise.
-    /// Mainly used for testing.
     ///
     /// - Returns: True if name, minimum, maximum and domain are equal.
     public static func == (lhs: Variable, rhs: Variable) -> Bool {
