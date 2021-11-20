e5533b14dd118a53f4c4e837200aed2c36180975
Michel Tilman <mtilman@acm.org>
2021-11-20 14:15:42 +0100

    Add variant on scoring a wedding seating.

--- Tests/SwiftPuLPTests/Examples/WeddingSeatingTests.swift
+++ Tests/SwiftPuLPTests/Examples/WeddingSeatingTests.swift
@@ -22,7 +22,7 @@ final class WeddingSeatingTests: XCTestCase {
     let maxTableSize = 4
     
     // The guests.
-    let guests = ["Athena", "Brienna", "Cordelia", "Darla", "Eliza", "Freya", "Gemma", "Inara", "Jemma", "Kara", "Leia", "Melissa", "Naya", "Olivia", "Paula", "Qira", "Reina"]
+    let guests = ["Athena", "Brienna", "Cordelia", "Darla", "Eliza", "Freya", "Gemma", "Inara", "Jasika", "Kara", "Leia", "Melissa", "Naya", "Olivia", "Paula", "Qira", "Reina"]
     
     // The binary variables, each one representing a possible table assignment.
     // Use tuples to keep track of the guests for each table.
@@ -30,11 +30,21 @@ final class WeddingSeatingTests: XCTestCase {
         (table: table, variable: Variable("table_\(table.joined(separator: "_")))", domain: .binary))
     }
     
-    func testSolveWeddingSeatingModel() {
-        let model = Model("Seating_Model", objective: objective(), constraints: constraints())
+    func testSolveHappinessModel() {
+        let model = Model("Happiness", objective: objective(happiness), constraints: constraints())
         guard let result = Solver().solve(model) else { return XCTFail("Nil result") }
         let tables = x.filter { (_, variable) in result.variables[variable.name] == 1 }.map(\.table)
-        let expected = [["Athena", "Melissa", "Qira"], ["Brienna", "Freya", "Paula"], ["Cordelia", "Leia", "Reina"], ["Darla", "Gemma", "Jemma", "Naya"], ["Eliza", "Inara", "Kara", "Olivia"]]
+        let expected = [["Athena", "Melissa", "Qira"], ["Brienna", "Freya", "Paula"], ["Cordelia", "Leia", "Reina"], ["Darla", "Gemma", "Jasika", "Naya"], ["Eliza", "Inara", "Kara", "Olivia"]]
+
+        XCTAssertEqual(result.status, .optimal)
+        XCTAssertTrue(tables.elementsEqual(expected, by: ==))
+    }
+
+    func testSolveLikesModel() {
+        let model = Model("Likes", objective: objective(likes), constraints: constraints())
+        guard let result = Solver().solve(model) else { return XCTFail("Nil result") }
+        let tables = x.filter { (_, variable) in result.variables[variable.name] == 1 }.map(\.table)
+        let expected = [["Athena", "Kara", "Melissa"], ["Cordelia", "Darla", "Eliza"], ["Paula", "Qira", "Reina"], ["Brienna", "Inara", "Leia", "Naya"], ["Freya", "Gemma", "Jasika", "Olivia"]]
 
         XCTAssertEqual(result.status, .optimal)
         XCTAssertTrue(tables.elementsEqual(expected, by: ==))
@@ -42,9 +52,9 @@ final class WeddingSeatingTests: XCTestCase {
 
     // MARK: Utility functions
     
-    // Maximizes total happiness across all table assignments.
-    private func objective() -> Model.Objective {
-        Model.Objective(Function.sum(x.map { (table, variable) in happiness(table) * variable }), optimization: .maximize)
+    // Maximizes total score across all table assignments.
+    private func objective(_ score: ([String]) -> Double) -> Model.Objective {
+        Model.Objective(Function.sum(x.map { (table, variable) in score(table) * variable }), optimization: .maximize)
     }
                         
     // Limits the maximum number of tables.
@@ -60,11 +70,28 @@ final class WeddingSeatingTests: XCTestCase {
         return constraints
     }
     
-    // Dummy 'happiness' calculation.
+    // Dummy 'happiness' calculation from PuLP example.
     // Answers the happiness of the table guests by calculating the maximum distance between the initial letters of each guest.
     // Guests are sorted by name.
     private func happiness(_ table: [String]) -> Double {
         abs(Double(table[0].first?.asciiValue ?? UInt8.min) - Double(table[table.count - 1].first?.asciiValue ?? UInt8.max))
     }
 
+    // Counts the number of likes in a table assignment.
+    private func likes(_ table: [String]) -> Double {
+        let likes = [
+            "Athena": ["Kara"],
+            "Eliza": ["Cordelia", "Darla"],
+            "Freya": ["Gemma", "Jasika"],
+            "Inara": ["Leia"],
+            "Kara": ["Athena"],
+            "Olivia": ["Jasika"],
+            "Qira": ["Paula"]
+        ]
+
+        return Double(table.reduce(into: 0) { total, guest in
+            if let friends = likes[guest] { total += Set(table).subtracting([guest]).intersection(friends).count }
+        })
+    }
+    
 }
